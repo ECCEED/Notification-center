489517b98fbf12cb5dd0c6e3d2b4db969d9df345
louay chayeb <117898415+ECCEED@users.noreply.github.com>
2024-07-11 03:00:32 +0100

    implementing CKEditor

---
+++ front/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+      <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+  </div>
+</template>
+
+<script>
+import { ClassicEditor, Bold, Essentials, Italic, Paragraph, Undo } from 'ckeditor5';
+import CKEditor from '@ckeditor/ckeditor5-vue';
+
+import 'ckeditor5/ckeditor5.css';
+import 'ckeditor5-premium-features/ckeditor5-premium-features.css';
+
+export default {
+  name: 'app',
+  components: {
+      ckeditor: CKEditor.component
+  },
+  data() {
+      return {
+          editor: ClassicEditor,
+          editorData: '<p>Hello!</p>',
+          editorConfig: {
+            plugins: [ Bold, Essentials, Italic, Paragraph, Undo ],
+            toolbar: [ 'undo', 'redo', '|', 'bold', 'italic' ]
+          }
+      };
+  }
+}
+</script>
