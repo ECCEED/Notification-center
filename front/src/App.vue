e62acc086e52f780d32fc7b0bc5f3a9b4318788c
louay chayeb <117898415+ECCEED@users.noreply.github.com>
2024-07-11 18:48:39 +0100

    ckeditor in components

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,14 +1,20 @@
 <template>
+  <div>
+    NEOLEDGE
+  </div>
   <div id="app">
       <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
   </div>
 </template>
 
 <script>
+
 import { ClassicEditor, Bold, Essentials, Italic, Paragraph, Undo } from 'ckeditor5';
+
 import CKEditor from '@ckeditor/ckeditor5-vue';
 
 import 'ckeditor5/ckeditor5.css';
+
 import 'ckeditor5-premium-features/ckeditor5-premium-features.css';
 
 export default {
