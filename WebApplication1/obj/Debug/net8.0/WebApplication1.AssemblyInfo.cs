b57bc6a1093542e4ab3e87af99609f844de8d0e0
louay chayeb <117898415+ECCEED@users.noreply.github.com>
2024-07-16 23:19:09 +0100

    16/07

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d29a513d407b1931cd9027c78f916249b31acc1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3303d8395fcc6b23234e3daabad43a1ea3dc24f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
